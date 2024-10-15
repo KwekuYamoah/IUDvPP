587eb57dd79b95d9b1edd137c40103e8c3df777c
benedictquartey <benedict_quartey@brown.edu>
2024-10-15 19:07:37 -0400

    prompt updates

--- ltl/llm_utils.py
+++ ltl/llm_utils.py
@@ -18,7 +18,7 @@ def build_intent_dict(intent_labels):
     return intent
 
 def generate_response_from_gpt4(user_prompt=None, deterministic=True):
-    gpt_model = "gpt-4o" # gpt-4 or gpt-4o
+    gpt_model = "gpt-4" # gpt-4 or gpt-4o
     print(f"\t\tModel: {gpt_model} || Deterministic?: {deterministic}")
     if deterministic == True:
         completion = openai.ChatCompletion.create(
@@ -46,15 +46,32 @@ def generate_response_from_gpt4(user_prompt=None, deterministic=True):
     return response
 
 
-def limp_llm_plan(limp_plan_incontext_prompt, instruction, intent_dict, verbose=False):
-    prompt_meat = f'''
-    Input_instruction:  {instruction}
-    Goal_intents: {intent_dict['Goal_intent']},
-    Avoidance_intent: {intent_dict['Avoidance_intent']},
-    Detail_intent: {intent_dict['Detail_intent']}
-    Output: '''
+def limp_llm_plan(limp_plan_incontext_prompt, instruction, intent_dict, baseline=None, verbose=False):
+    print(f"Executing Baseline: {baseline}")
+
+    if "intent_label" in baseline:
+        prompt_meat = f'''
+        Input_instruction:  {instruction}
+        Goal_intents: {intent_dict['Goal_intent']},
+        Avoidance_intent: {intent_dict['Avoidance_intent']},
+        Detail_intent: {intent_dict['Detail_intent']}
+        Output: '''
+    elif "asr" in baseline:
+        prompt_meat = f'''
+        Input_instruction:  {instruction}
+        Output: '''
+    elif "prosody_value" in baseline:
+        prompt_meat = f'''
+        Input_instruction:  {instruction}
+        prosody_values: 
+        Output: '''
+    else:
+        #error exit
+        print("Error: Baseline not found")
+        return None
+
     complete_prompt = limp_plan_incontext_prompt + prompt_meat
-    print(f"Prompt: {complete_prompt}") if verbose else None
+    print(f"Complete prompt:\n {complete_prompt}") if verbose else None
     response = generate_response_from_gpt4(complete_prompt)
     return response
 
